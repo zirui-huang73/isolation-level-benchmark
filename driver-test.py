11b223e99eef5634a8decf0129154a93a12d017e
KSDeng <kaisheng.deng@outlook.com>
2022-03-26 14:31:57 +0800

    connect using config file

--- driver-test.py
+++ driver-test.py
@@ -1,12 +1,15 @@
 import psycopg2
+from init import config
+
 
 if __name__ == '__main__':
     conn = None
 
     try:
-        conn = psycopg2.connect("dbname=supplier user=postgres password=postgresqldks")
-        cur = conn.cursor()
+        params = config()
 
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
         cur.execute('SELECT version()')
         db_version = cur.fetchone()
         print(db_version)
