b06184534319556696bffd3ecf44a5c8f7a58524
KSDeng <kaisheng.deng@outlook.com>
2022-03-26 16:47:01 +0800

    modify main driver

--- mainDriver.py
+++ mainDriver.py
@@ -1,12 +1,26 @@
 import sys
 import psycopg2
 from init import config
-from src import lost_update
+from src import lost_update, phantom_read
+
+functionMap = {
+    'Lost-Update': [lost_update.transaction1, lost_update.transaction2],
+    'Phantom-Read': [phantom_read.transaction1, phantom_read.transaction2],
+}
+
+isolationMap = {
+    'RC': 'READ COMMITTED',
+    'RR': 'REPEATABLE READ',
+    'S': 'Serializable'
+}
 
 """
 @:param1: Anomaly Type
     Lost-Update
-    ...
+    Non-Repeatable-Read
+    Phantom-Read
+    Read-Skew
+    Write-Skew
 @:param2: Isolation Level
     RC: read committed
     RR: repeatable read
@@ -14,31 +28,20 @@ from src import lost_update
 """
 if __name__ == '__main__':
 
-    TYPE = sys.argv[1]
-    IL = sys.argv[2]
-    print(TYPE)
-    print(IL)
+    TYPE = sys.argv[1]          # anomaly type
+    IL = sys.argv[2]            # isolation level
 
     conn = None
-
     try:
         params = config()
-
         conn = psycopg2.connect(**params)
 
-        if IL == 'RC':
-            conn.set_session(isolation_level='READ COMMITTED', autocommit=False)
-            if TYPE == 'Lost-Update':
-                lost_update.transaction1(conn)
-                lost_update.transaction2(conn)
-        elif IL == 'RR':
-            print('RR')
+        isolationLevel = isolationMap[IL]
+        funcs = functionMap[TYPE]
 
-        elif IL == 'S':
-            print('S')
-
-        else:
-            raise Exception('Isolation level {0} not known'.format(IL))
+        conn.set_session(isolation_level=isolationLevel, autocommit=False)
+        for func in funcs:
+            func(conn)
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
