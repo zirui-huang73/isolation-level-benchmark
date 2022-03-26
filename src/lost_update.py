595ce986fbe2406d1f4d4225e718301d6d7d89a2
ziruih <ziruih73@gmail.com>
2022-03-26 17:49:42 +0800

    update PR and NR

--- src/lost_update.py
+++ src/lost_update.py
@@ -2,8 +2,8 @@ from psycopg2 import *
 from .transaction import Transaction
 
 def run(conn):
-    transaction_1 = ['select cash from count where id = 1', 'update count set cash = 50 where id = 1']
-    transaction_2 = ['select cash from count where id = 1', 'update count set cash = 80 where id = 1']
+    transaction_1 = ['select x from cal where id = 1', 'update cal set x = 50 where id = 1']
+    transaction_2 = ['select x from cal where id = 1', 'update cal set x = 80 where id = 1']
     transaction = Transaction(conn=conn, transaction_1=transaction_1, transaction_2=transaction_2)
     result = transaction.exec()
     print(result)
