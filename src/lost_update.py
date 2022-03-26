7fc60091b849c3e972c8df49db5a8cbeca494199
Fangzhou Lan <fangzhou.lan@shopee.com>
2022-03-26 18:08:01 +0800

    Update transaction class

--- src/lost_update.py
+++ src/lost_update.py
@@ -1,9 +1,26 @@
 from psycopg2 import *
 from .transaction import Transaction
 
-def run(conn):
-    transaction_1 = ['select cash from count where id = 1', 'update count set cash = 50 where id = 1']
-    transaction_2 = ['select cash from count where id = 1', 'update count set cash = 80 where id = 1']
-    transaction = Transaction(conn=conn, transaction_1=transaction_1, transaction_2=transaction_2)
-    result = transaction.exec()
-    print(result)
+class transaction1(Transaction):
+    def __init__(self, conn):
+        super().__init__(conn)
+        
+    def process(self):
+        cur = self.conn.cursor()
+        cur.execute('begin')
+        cur.execute('select cash from count where id = 1')
+        cur.execute('update count set cash = 50 where id = 1')
+        self.conn.commit()
+        cur.close()
+
+class transaction2(Transaction):
+    def __init__(self, conn):
+        super().__init__(conn)
+        
+    def process(self):
+        cur = self.conn.cursor()
+        cur.execute('begin')
+        cur.execute('select cash from count where id = 1')
+        cur.execute('update count set cash = 80 where id = 1')
+        self.conn.commit()
+        cur.close()
--- src/transaction.py
+++ src/transaction.py
@@ -1,38 +1,32 @@
 from psycopg2 import *
-import random
 import time
 
 class Transaction(object):
-    def __init__(self, conn, transaction_1, transaction_2):
+    def __init__(self, conn):
         self.conn = conn
-        self.transaction_1 = transaction_1
-        self.transaction_2 = transaction_2
         self.elapsed_time = -1
         self.num_retry = 0
-        self.return_status = "fail"
+        self.return_status = False
+
+    def process(self):
+        pass
 
     def exec(self):
-        rand = random.randint(0, 1)
         start_time = time.perf_counter()
         while True:
             try:
-                cur = self.conn.cursor()
-                cur.execute('begin')
-                if (rand == 0):
-                    for sql in self.transaction_1:
-                        cur.execute(sql)
-                else:
-                    for sql in self.transaction_2:
-                        cur.execute(sql)
-                self.conn.commit()
                 self.elapsed_time = time.perf_counter() - start_time
-                self.conn.close()
-                self.return_status = "pass"
+                if self.num_retry == 3:
+                    raise Exception("fail with too much retry(3 times)")
+                self.process()
+                self.return_status = True
                 break
             except OperationalError as error:
                 self.conn.rollback()
                 self.num_retry += 1
-                print("get operational error {}".format(error))
+                sleep_time: float = self.num_retry * 0.1
+                time.sleep(sleep_time)
+                print("get operational error {}, sleep {} seconds".format(error, sleep_time))
                 continue
             except (Exception, DatabaseError) as error:
                 print("get other error {}".format(error))
