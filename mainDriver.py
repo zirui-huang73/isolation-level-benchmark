09f352261b007dc60b79d2f226572a457868be13
KSDeng <kaisheng.deng@outlook.com>
2022-03-28 22:18:37 +0800

    write statistics to file

--- mainDriver.py
+++ mainDriver.py
@@ -3,7 +3,9 @@ import psycopg2
 from init import config
 from src import lost_update, phantom_read, nonrepeatable_read, Read_Skew, Write_Skew
 import random
-
+import json
+import os
+from datetime import datetime
 import logging
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
@@ -39,28 +41,36 @@ if __name__ == '__main__':
     TYPE = sys.argv[1]          # anomaly type
     IL = sys.argv[2]            # isolation level
 
+    N = 10
+
     conn = None
     try:
         params = config()
         conn = psycopg2.connect(**params)
 
         isolationLevel = isolationMap[IL]
-        TransactionClasses = TransactionClassMap[TYPE]
-
-        index = random.randint(0, len(TransactionClasses) - 1)
-        TransactionClass = TransactionClasses[index]
-
         conn.set_session(isolation_level=isolationLevel, autocommit=False)
 
-        transaction = TransactionClass(conn)
-        result = transaction.exec()
-        logging.warning("transaction performance result: {}".format(result))
+        TransactionClasses = TransactionClassMap[TYPE]
+
+        result = {}
+        for i in range(N):
+            index = random.randint(0, len(TransactionClasses) - 1)
+            TransactionClass = TransactionClasses[index]
+            transaction = TransactionClass(conn)
+            result[i] = transaction.exec()
+
+        # write results to json file
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+        date_time = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+        with open('logs/{0}-{1}-{2}.json'.format(TYPE, IL, date_time), 'w') as outputFile:
+            json.dump(result, outputFile)
 
     except (Exception, psycopg2.DatabaseError) as error:
         logging.exception(error)
     finally:
         if conn is not None:
             conn.close()
-
 
 
